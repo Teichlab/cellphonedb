244779caf1305972f3aff4f549e2d00f12128b99
Mvento <miquel@emevento.com>
2017-09-26 22:43:26 +0200

    solved problems in complex csv file. Removed useless columns.

--- cellcommdb/collections/complex.py
+++ cellcommdb/collections/complex.py
@@ -25,7 +25,7 @@ def load(complex_file=None):
     proteins = db.session.query(Multidata.name, Multidata.id).join(Protein).all()
     proteins = {p[0]: p[1] for p in proteins}
     # Read in complexes
-    complex_df = pd.read_csv(complex_file, quotechar='"', na_values="NA", sep=',')
+    complex_df = pd.read_csv(complex_file, quotechar='"', na_values="NA", sep=';')
     complex_df.dropna(axis=1, inplace=True, how='all')
     complex_df.rename(index=str, columns={'complex_name': 'name'}, inplace=True)
 
