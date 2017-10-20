5ad6e6c53f9440caec0a7ebe1929cd27d804e2a4
Mvento <miquel@emevento.com>
2017-10-20 15:09:46 +0200

    Converted to new csv protein format

--- cellcommdb/collections/protein.py
+++ cellcommdb/collections/protein.py
@@ -18,6 +18,42 @@ def load(protein_file=None):
 
     csv_proteins_df = pd.read_csv(protein_file)
 
+    csv_proteins_df[bools] = csv_proteins_df[bools].astype(bool)
+
+    insert_multidata_proteins(csv_proteins_df)
+    insert_proteins_in_db(csv_proteins_df)
+
+
+def insert_multidata_proteins(proteins):
+    multidata_columns = database.get_column_table_names(Multidata, db)
+    multidata_proteins = proteins.copy()
+    multidata_proteins.rename(index=str, columns={'uniprot': 'name'}, inplace=True)
+    multidata_proteins = filters.remove_not_defined_columns(multidata_proteins, multidata_columns)
+    multidata_proteins.to_sql(name='multidata', if_exists='append', con=db.engine, index=False)
+
+
+def insert_proteins_in_db(proteins_df):
+    multidatas = pd.read_sql_table(table_name='multidata', con=db.engine)
+    multidatas.rename(index=str, columns={'id': 'protein_multidata_id'}, inplace=True)
+    proteins = proteins_df.copy()
+    proteins.rename(index=str, columns={'uniprot': 'name'}, inplace=True)
+
+    proteins = pd.merge(proteins, multidatas, on='name')
+
+    proteins = filters.remove_not_defined_columns(proteins, database.get_column_table_names(Protein, db))
+    proteins.to_sql(name='protein', if_exists='append', con=db.engine, index=False)
+
+
+def old_load(protein_file=None):
+    bools = ['transmembrane', 'secretion', 'peripheral', 'receptor',
+             'receptor_highlight', 'adhesion', 'other', 'transporter',
+             'secreted_highlight', 'ligand', 'adaptor']
+
+    if not protein_file:
+        protein_file = os.path.join(current_dir, 'data', 'protein.csv')
+
+    csv_proteins_df = pd.read_csv(protein_file)
+
     _clear_column(csv_proteins_df, 'id')
 
     csv_proteins_df[bools] = csv_proteins_df[bools].astype(bool)
@@ -38,7 +74,6 @@ def load(protein_file=None):
     _defineProteinType(unique_prots, 'GPI-anchor.csv', 'GPI-anchor')
 
     _save_proteins_in_db(unique_prots)
-
 
 
 def _defineProteinType(proteins_df, protein_type_file, type_name):
