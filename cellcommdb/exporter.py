f5384cb11e36be4cf17e4609166ba5e8baf7ec77
Mvento <miquel@emevento.com>
2017-10-24 01:06:59 +0200

    added entry_name to complex exporter

--- cellcommdb/exporter.py
+++ cellcommdb/exporter.py
@@ -45,10 +45,14 @@ class Exporter(object):
             complex_query = db.session.query(Complex)
             multidata_query = db.session.query(Multidata)
             complex_composition_query = db.session.query(ComplexComposition)
+            protein_query = db.session.query(Protein, Multidata).join(Multidata)
 
             complex_df = pd.read_sql(complex_query.statement, db.engine)
             multidata_df = pd.read_sql(multidata_query.statement, db.engine)
             complex_composition_df = pd.read_sql(complex_composition_query.statement, db.engine)
+            protein_df = pd.read_sql(protein_query.statement, db.engine)
+
+            print(protein_df)
 
             complex_complete = pd.merge(complex_df, multidata_df, left_on='complex_multidata_id', right_on='id')
 
@@ -58,17 +62,28 @@ class Exporter(object):
                     complex_composition_df['complex_multidata_id'] == complex['complex_multidata_id']]
 
                 protein_index = 1
-                complex_proteins = {'complex_multidata_id': complex['complex_multidata_id'], 'protein_1': None,
-                                    'protein_2': None, 'protein_3': None, 'protein_4': None}
+                complex_proteins = {'complex_multidata_id': complex['complex_multidata_id'],
+                                    'protein_1': None, 'protein_1_gene_name': None,
+                                    'protein_2': None, 'protein_2_gene_name': None,
+                                    'protein_3': None, 'protein_3_gene_name': None,
+                                    'protein_4': None, 'protein_4_gene_name': None
+                                    }
                 for index, complex_composition in complex_complex_composition.iterrows():
-                    proteine_name = multidata_df.iloc[complex_composition['protein_multidata_id'], :]['name']
+                    proteine_name = \
+                    multidata_df[multidata_df['id'] == complex_composition['protein_multidata_id']]['name'].values[0]
                     complex_proteins['protein_%i' % protein_index] = proteine_name
+
+                    entry_name = protein_df[protein_df['name'] == proteine_name]['entry_name'].values[0]
+                    print('->protein_name: %s' % proteine_name)
+                    print(entry_name)
+                    complex_proteins['protein_%i_gene_name' % protein_index] = entry_name
                     protein_index += 1
 
                 composition.append(complex_proteins)
 
             complex_complete = pd.merge(complex_complete, pd.DataFrame(composition), on='complex_multidata_id')
             complex_complete.drop(['id_x', 'id_y', 'complex_multidata_id'], axis=1, inplace=True)
+
             complex_complete.to_csv('out/%s' % output_name, index=False)
 
     def gene(self, output_name=None):
