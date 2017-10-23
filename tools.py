2e0b6b57adf961747f141f8582948a4e2a84972c
Mvento <miquel@emevento.com>
2017-10-23 21:43:44 +0200

    created tools out folder

--- tools.py
+++ tools.py
@@ -3,6 +3,7 @@ from flask_script import Manager
 from tools.app import create_app
 from tools.merge_duplicated_proteins import merge_duplicated_proteins as merge_proteins
 from tools.merge_gene_mouse import merge_gene_mouse as merge_gene
+
 app = create_app()
 manager = Manager(app)
 
