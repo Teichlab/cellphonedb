5563b2f7e1392f26dabe0e92374588207ec32f53
Mvento <miquel@emevento.com>
2017-11-03 10:12:59 +0100

    Created endpoint for query 1

--- cellcommdb/api.py
+++ cellcommdb/api.py
@@ -1,6 +1,6 @@
 import os
 
-from cellcommdb.api_endpoints.queries import Query0Endpoint
+from cellcommdb.api_endpoints.queries import Query0Endpoint, Query1Endpoint
 from flask import Flask, abort, request
 from flask_restful import Api, Resource
 
@@ -24,5 +24,6 @@ def create_app(config=BaseConfig):
     # api.add_resource(ComplexResource, '/complex')
 
     api.add_resource(Query0Endpoint, '/query0')
+    api.add_resource(Query1Endpoint, '/query1')
 
     return app
--- cellcommdb/api_endpoints/queries.py
+++ cellcommdb/api_endpoints/queries.py
@@ -3,17 +3,24 @@ from flask import request, Response
 from flask_restful import Resource, reqparse
 
 from cellcommdb.queries.query0 import Query0
+from cellcommdb.queries.query1 import Query1
 
 parser = reqparse.RequestParser()
 parser.add_argument('meta')
 parser.add_argument('counts')
 
 
-# For Test
+# Example query
+# Query0
 # curl -i \
 #     -F counts_file=@test_counts.txt \
 #     -F meta_file=@test_meta.txt \
 #     http://127.0.0.1:5000/api/query0
+#
+# Query1
+# curl -i \
+#     -F processed_data=@query_0.csv \
+#     http://127.0.0.1:5000/api/query1
 
 class Query0Endpoint(Resource):
     def post(self):
@@ -21,5 +28,14 @@ class Query0Endpoint(Resource):
         meta_df = pd.read_csv(request.files['meta_file'].stream, sep='\t')
 
         result_df = Query0.call(counts_df, meta_df)
+
+        return Response(result_df.to_csv(index=False), mimetype='text/csv')
+
+
+class Query1Endpoint(Resource):
+    def post(self):
+        processed_data = pd.read_csv(request.files['processed_data'].stream)
+
+        result_df = Query1.call(processed_data)
 
         return Response(result_df.to_csv(index=False), mimetype='text/csv')
