bc0ea1d64a15a03d966ffa7161c5cadcf7303410
Mvento <miquel@emevento.com>
2017-11-05 23:00:05 +0100

    removed predefined namefiles

--- cellcommdb/query_manager.py
+++ cellcommdb/query_manager.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from cellcommdb.api import current_dir, output_dir
+from cellcommdb.api import current_dir, output_dir, data_dir
 from cellcommdb.queries.query0 import Query0
 from cellcommdb.queries.query1 import Query1
 from cellcommdb.queries.query2 import Query2
@@ -11,11 +11,8 @@ class Queries(object):
         self.app = app
 
     def query0(self, counts_namefile, meta_namefile):
-        counts_namefile = 'test_counts.txt'
-        meta_namefile = 'test_meta.txt'
-
-        counts_df = pd.read_csv('%s/data/%s' % (current_dir, counts_namefile), sep='\t')
-        meta_df = pd.read_csv('%s/data/%s' % (current_dir, meta_namefile), sep='\t')
+        counts_df = pd.read_csv('%s%s' % (data_dir, counts_namefile), sep='\t')
+        meta_df = pd.read_csv('%s%s' % (data_dir, meta_namefile), sep='\t')
 
         result_df = Query0.call(counts_df, meta_df)
 
