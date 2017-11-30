9139dc5a2c2d3400661a55771aff8556c3d7d753
Mvento <miquel@emevento.com>
2017-11-30 14:29:41 +0100

    ReceptorLigandsInteractions RESTful api updated

--- cellcommdb/api_endpoints/queries.py
+++ cellcommdb/api_endpoints/queries.py
@@ -1,10 +1,9 @@
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 import pandas as pd
-from flask import request, Response, jsonify
-from flask_restful import Resource, reqparse
+from flask import request, Response
+from flask_restful import Resource
 from cellcommdb.queries import cells_to_clusters, receptor_ligands_interactions
 
 import json
@@ -49,19 +48,15 @@ class CellToCluster(QueryBase):
 
         result_df = cells_to_clusters.call(counts, meta)
 
+        self._attach_json({'status': 'nice'})
         self._attach_csv(result_df.to_csv(sep='\t'), 'cluster_counts.csv')
 
-        result = {
-            'status': 'nice'
-        }
-
-        self._attach_json(result)
         return Response(self.msg.as_string())
 
 
 class ReceptorLigandsInteractions(QueryBase):
     def post(self):
-        cells_to_clusters_file = pd.read_csv(request.files['cells_to_clusters'].stream, index_col=0)
+        cells_to_clusters_file = pd.read_table(request.files['cell_to_clusters_file'].stream, index_col=0)
 
         parameters = json.loads(request.form['parameters'])
         threshold = float(parameters['threshold'])
@@ -69,6 +64,7 @@ class ReceptorLigandsInteractions(QueryBase):
         result_interactions, result_interactions_extended = receptor_ligands_interactions.call(cells_to_clusters_file,
                                                                                                threshold)
 
+        self._attach_json({'status': 'nice'})
         self._attach_csv(result_interactions.to_csv(sep='\t'), 'result_interactions.csv')
         self._attach_csv(result_interactions_extended.to_csv(sep='\t'), 'result_interactions_extended.csv')
 
