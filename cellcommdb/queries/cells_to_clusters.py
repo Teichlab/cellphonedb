40a6f479b540d43b6c375a4d4dc4dcc62c88b65e
Mvento <miquel@emevento.com>
2017-12-06 16:03:50 +0100

    Cells to clusters prefiltered by database cells

--- cellcommdb/queries/cells_to_clusters.py
+++ cellcommdb/queries/cells_to_clusters.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
+from cellcommdb.extensions import db
+from cellcommdb.models import Gene, Protein, Multidata
+
 
 def call(counts, meta):
-    clusters = _create_clusters_structure(counts, meta)
+    cellphone_counts = _filter_by_cellphone_genes(counts)
+    clusters = _create_clusters_structure(cellphone_counts, meta)
 
     return _clusters_ratio(clusters)
 
 
-def _clusters_ratio(clusters):
-    all_cells_names = next(iter(clusters.values())).index
+def _clusters_ratio(counts):
+    all_cells_names = next(iter(counts.values())).index
 
     result = pd.DataFrame(None, all_cells_names)
-    for cluster_name in clusters:
+    for cluster_name in counts:
         print('Transforming Cluster %s' % cluster_name)
-        cluster = clusters[cluster_name]
+        cluster = counts[cluster_name]
 
         cells_names = cluster.columns.values
         number_cells = len(cells_names)
@@ -21,6 +25,23 @@ def _clusters_ratio(clusters):
         result[cluster_name] = cluster_count_value
 
     return result
+
+
+def _filter_by_cellphone_genes(cluster_counts):
+    '''
+    Merges cluster genes with CellPhoneDB values
+    :type cluster_counts: pd.DataFrame
+    :rtype: pd.DataFrame
+    '''
+    gene_protein_query = db.session.query(Gene.ensembl)
+    gene_protein_df = pd.read_sql(gene_protein_query.statement, db.engine)
+
+    gene_protein_df.rename(columns={'id': 'multidata_id', 'ensembl': 'Gene'}, inplace=True)
+
+    multidata_counts = pd.merge(cluster_counts, gene_protein_df, left_index=True, right_on='Gene')
+
+    multidata_counts.set_index('Gene', inplace=True)
+    return multidata_counts
 
 
 def _create_clusters_structure(counts, meta):
