80b5925d84712aa073cf6ff669623e195ba035b8
Mvento <miquel@emevento.com>
2017-12-12 10:48:17 +0100

    renamed models ids to id_modelname format

--- cellcommdb/models.py
+++ cellcommdb/models.py
@@ -4,12 +4,9 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Float, Binary, \
 from cellcommdb.extensions import db
 
 
-class IdModel(object):
-    id = Column(Integer, nullable=False, primary_key=True)
-
-
-class Multidata(db.Model, IdModel):
+class Multidata(db.Model):
     __tablename__ = 'multidata'
+    id_multidata = Column(Integer, nullable=False, primary_key=True)
 
     name = Column(String, nullable=False, unique=True)
 
@@ -33,40 +30,45 @@ class Multidata(db.Model, IdModel):
     complex = db.relationship('Complex', backref='complex', lazy='subquery')
 
 
-class Protein(db.Model, IdModel):
+class Protein(db.Model):
     __tablename__ = 'protein'
+    id_protein = Column(Integer, nullable=False, primary_key=True)
 
     entry_name = Column(String)
     tags = Column(String)
     tags_reason = Column(String)
 
-    protein_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id'), unique=True, nullable=False)
+    protein_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id_multidata'), unique=True, nullable=False)
     gene = db.relationship('Gene', backref='gene', lazy='subquery')
 
 
-class ComplexComposition(db.Model, IdModel):
+class ComplexComposition(db.Model):
     __tablename__ = 'complex_composition'
+    id_complex_composition = Column(Integer, nullable=False, primary_key=True)
 
-    complex_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id'), nullable=False)
-    protein_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id'), nullable=False)
+    complex_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id_multidata'), nullable=False)
+    protein_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id_multidata'), nullable=False)
     total_protein = Column(Integer)
 
 
-class Complex(db.Model, IdModel):
+class Complex(db.Model):
     __tablename__ = 'complex'
+    id_complex = Column(Integer, nullable=False, primary_key=True)
 
-    complex_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id'), nullable=False, unique=True)
+    complex_multidata_id = db.Column(db.Integer, db.ForeignKey('multidata.id_multidata'), nullable=False, unique=True)
     pdb_structure = Column(String)
     pdb_id = Column(String)
     stoichiometry = Column(String)
     comments = Column(String)
 
 
-class Interaction(db.Model, IdModel):
+class Interaction(db.Model):
     __tablename__ = 'interaction'
 
-    multidata_1_id = db.Column(db.Integer, db.ForeignKey('multidata.id'), nullable=False)
-    multidata_2_id = db.Column(db.Integer, db.ForeignKey('multidata.id'), nullable=False)
+    id_interaction = Column(Integer, nullable=False, primary_key=True)
+
+    multidata_1_id = db.Column(db.Integer, db.ForeignKey('multidata.id_multidata'), nullable=False)
+    multidata_2_id = db.Column(db.Integer, db.ForeignKey('multidata.id_multidata'), nullable=False)
 
     score_1 = db.Column(Float)
     score_2 = db.Column(Float)
@@ -75,10 +77,13 @@ class Interaction(db.Model, IdModel):
     comments = db.Column(String)
 
 
-class Gene(db.Model, IdModel):
+class Gene(db.Model):
     __tablename__ = 'gene'
+    id_gene = Column(Integer, nullable=False, primary_key=True)
+
+    gene_id = Column(Integer, primary_key=True)
 
     ensembl = Column(String, nullable=False)
     gene_name = Column(String, nullable=False)
 
-    protein_id = Column(Integer, ForeignKey('protein.id'))
+    protein_id = Column(Integer, ForeignKey('protein.id_protein'))
