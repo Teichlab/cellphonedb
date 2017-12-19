0c0eb1661975d9f0b9a21547ff49a2fef0888c2a
Mvento <miquel@emevento.com>
2017-12-19 12:20:51 +0100

    python test for multidata is receptor/ligand

---
+++ cellcommdb/tests/models/test_properties_multidata.py
@@ -0,0 +1,73 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from cellcommdb.models.multidata import properties_multidata
+
+
+class TestPropertiesMultidata(TestCase):
+    def test_is_ligand(self):
+        multidatas = pd.DataFrame(multidatas_fixtures)
+
+        for index, multidata in multidatas.iterrows():
+            self.assertEqual(properties_multidata.is_ligand(multidata), multidata['is_ligand'],
+                             'Multidata %s is not ligand' % multidata['id'])
+
+    def test_is_receptor(self):
+        multidatas = pd.DataFrame(multidatas_fixtures)
+        for index, multidata in multidatas.iterrows():
+            self.assertEqual(properties_multidata.is_receptor(multidata), multidata['is_receptor'])
+
+
+multidatas_fixtures = [
+    {
+        'id': 1,
+        'receptor': True,
+        'secretion': True,
+        'other': False,
+        'transmembrane': False,
+        'extracellular': False,
+        'cytoplasm': False,
+        'transporter': False,
+        'is_ligand': True,
+        'is_receptor': False
+
+    },
+    {
+        'id': 2,
+        'receptor': True,
+        'secretion': False,
+        'other': False,
+        'transmembrane': True,
+        'extracellular': True,
+        'cytoplasm': False,
+        'transporter': False,
+        'is_ligand': True,
+        'is_receptor': True
+
+    },
+    {
+        'id': 3,
+        'receptor': True,
+        'secretion': False,
+        'other': False,
+        'transmembrane': False,
+        'extracellular': False,
+        'cytoplasm': False,
+        'transporter': False,
+        'is_ligand': False,
+        'is_receptor': False
+    },
+    {
+        'id': 4,
+        'receptor': False,
+        'secretion': False,
+        'other': False,
+        'transmembrane': False,
+        'extracellular': False,
+        'cytoplasm': False,
+        'transporter': False,
+        'is_ligand': False,
+        'is_receptor': False
+    },
+]
