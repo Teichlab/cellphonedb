43cd58cf1ec63c88e696489607768606953753dc
Mvento <miquel@emevento.com>
2017-12-19 12:27:42 +0100

    get ligands when string correspond to multiple proteins

--- cellcommdb/api_endpoints/queries/get_ligands_from_receptor.py
+++ cellcommdb/api_endpoints/queries/get_ligands_from_receptor.py
@@ -1,34 +1,39 @@
 import json
 
+import pandas as pd
 from flask import request, Response
 
 from cellcommdb.api_endpoints.endpoint_base import EndpointBase
 from cellcommdb.queries import get_ligands_from_receptor
 
-# curl -i \
-#      --data "{\"receptor\": \"P25106\"}" \
-#      http://127.0.0.1:5000/api/get_ligands_from_receptor
 from cellcommdb.common.generic_exception import GenericException
 from cellcommdb.repository import multidata_repository
 
 
+# curl -i \
+#      --data "{\"receptor\": \"P25106\"}" \
+#      http://127.0.0.1:5000/api/get_ligands_from_receptor
 class GetLigandsFromReceptor(EndpointBase):
     def post(self):
         parameters = json.loads(request.get_data(as_text=True))
 
         receptor = parameters['receptor']
 
-        multidata_receptor = multidata_repository.get_multidata_from_string(receptor)
+        multidatas_receptors = multidata_repository.get_multidatas_from_string(receptor)
 
-        if multidata_receptor.empty:
+        if multidatas_receptors.empty:
             self.attach_error(
                 {'code': 'result_not_found', 'title': '%s not found' % receptor,
                  'details': '%s is not in Cellphone Database' % receptor})
 
         if not self._errors:
             try:
-                ligands = get_ligands_from_receptor.call(multidata_receptor.iloc[0], 0.3)
-                self._attach_table(ligands.to_csv(index=False, sep='\t'), 'ligands')
+                result = pd.DataFrame()
+                for index, multidata_receptor in multidatas_receptors.iterrows():
+                    ligands = get_ligands_from_receptor.call(multidata_receptor, 0.3)
+                    result = result.append(ligands, ignore_index=True)
+
+                self._attach_table(result.to_csv(index=False, sep='\t'), 'ligands')
             except GenericException as e:
                 self.attach_error(e.args[0])
 
