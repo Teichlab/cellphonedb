a79d094183e9b8927be543c5a967b946da8cb247
Mvento <miquel@emevento.com>
2017-12-28 17:43:40 +0100

    removed unused files

--- cellphonedb/query_manager.py
+++ cellphonedb/query_manager.py
@@ -1,13 +1,8 @@
-import mimetypes
-from email import encoders
-
 import pandas as pd
 
 from cellphonedb.api import current_dir, output_dir, query_input_dir
-from cellphonedb.queries.query0 import Query0
-from cellphonedb.queries.query1 import Query1
 
-from cellphonedb.queries import complexes, one_one, cells_to_clusters, \
+from cellphonedb.queries import cells_to_clusters, \
     receptor_ligands_interactions, get_rl_lr_interactions_from_multidata
 from cellphonedb.repository import multidata_repository
 
@@ -15,33 +10,6 @@ from cellphonedb.repository import multidata_repository
 class QueryLauncher(object):
     def __init__(self, app):
         self.app = app
-
-    def query0(self, counts_namefile, meta_namefile):
-        counts_df = pd.read_csv('%s/%s' % (query_input_dir, counts_namefile), sep='\t')
-        meta_df = pd.read_csv('%s/%s' % (query_input_dir, meta_namefile), sep='\t')
-
-        result_df = Query0.call(counts_df, meta_df)
-
-        result_df.to_csv('%squery_0.csv' % output_dir, index=False)
-
-    def query1(self, processed_data_namefile):
-        processed_data_namefile = 'query_0.csv'
-
-        processed_data_df = pd.read_csv('%s/data/%s' % (current_dir, processed_data_namefile))
-
-        result_df = Query1.call(processed_data_df)
-
-        result_df.to_csv('%s/query_1.csv' % output_dir, index=False)
-
-    def complexes(self, counts_namefile, meta_namefile):
-        counts = pd.read_table('%s/%s' % (query_input_dir, counts_namefile), index_col=0)
-        meta = pd.read_table('%s/%s' % (query_input_dir, meta_namefile), index_col=0)
-        complexes.call(counts, meta)
-
-    def one_one(self, counts_namefile, meta_namefile):
-        counts = pd.read_table('%s/%s' % (query_input_dir, counts_namefile), index_col=0)
-        meta = pd.read_table('%s/%s' % (query_input_dir, meta_namefile), index_col=0)
-        one_one.call(counts, meta)
 
     def cells_to_clusters(self, counts_namefile, meta_namefile):
         counts = pd.read_table('%s/%s' % (query_input_dir, counts_namefile), index_col=0)
