b63e1a4ad7f6823a0b9ae3ddec1336a3f9b42a91
Mvento <miquel@emevento.com>
2017-12-28 14:22:36 +0100

    find duplicated proteins

--- cellcommdb/models/interaction/functions_interaction.py
+++ cellcommdb/models/interaction/functions_interaction.py
@@ -12,3 +12,20 @@ def expand_interactions_multidatas(interactions: pd.DataFrame, suffixes: list = 
 
     interactions_expanded.drop_duplicates(inplace=True)
     return interactions_expanded
+
+
+def get_duplicated(interactions: pd.DataFrame, column_1: str, column_2: str) -> pd.DataFrame:
+    def duplicated(row):
+        if len(interactions[(row[column_1] == interactions[column_1]) & (
+                row[column_2] == interactions[column_2])]) > 1:
+            return True
+
+        if (row[column_1] != row[column_2]) & len(
+                interactions[(row[column_1] == interactions[column_2]) & (
+                        row[column_2] == interactions[column_1])]) > 0:
+            return True
+        return False
+
+    result = interactions[interactions.apply(duplicated, axis=1)]
+
+    return result
--- tools/generate_data/mergers/add_curated.py
+++ tools/generate_data/mergers/add_curated.py
@@ -1,13 +1,9 @@
+import pandas as pd
+
 from tools.generate_data.mergers.merge_interactions import merge_interactions
 
 
-def add_curated(interactions, interaction_curated):
-    '''
-
-    :type interactions: pd.DataFrame
-    :type interaction_curated: pd.DataFrame
-    :rtype: pd.DataFrame
-    '''
+def add_curated(interactions: pd.DataFrame, interaction_curated: pd.DataFrame) -> pd.DataFrame:
     interactions.rename(index=str, columns={'protein_1': 'multidata_name_1', 'protein_2': 'multidata_name_2'},
                         inplace=True)
 
