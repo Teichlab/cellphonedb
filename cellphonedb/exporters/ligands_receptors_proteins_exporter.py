c9221fe4f0275f021b544808a0abc8fd73655b10
Mvento <miquel@emevento.com>
2017-12-29 10:58:07 +0100

    added columns exracellular and cytoplasm

--- cellphonedb/exporters/ligands_receptors_proteins_exporter.py
+++ cellphonedb/exporters/ligands_receptors_proteins_exporter.py
@@ -7,8 +7,8 @@ def call():
     multidatas = filters_multidata.filter_by_lingand_or_receptor(multidatas)
     result = multidatas.loc[:, [
                                    'ensembl', 'name', 'entry_name', 'transmembrane', 'secretion', 'peripheral',
-                                   'is_cellphone_receptor',
-                                   'is_cellphone_ligand', 'transporter', 'other', 'other_desc', 'tags', 'tags_reason']]
+                                   'is_cellphone_receptor', 'is_cellphone_ligand', 'transporter', 'other', 'other_desc',
+                                   'tags', 'tags_reason', 'cytoplasm', 'extracellular']]
 
     result.rename(columns={'name': 'uniprot', 'secretion': 'secreted', 'peripheral': 'peripheral_proteins',
                            'is_cellphone_receptor': 'receptor_cellphone', 'is_cellphone_ligand': 'ligands_cellphone'},
