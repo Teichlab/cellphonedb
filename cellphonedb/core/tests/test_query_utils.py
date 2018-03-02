04e903f5bc470f70313f8d2cfb928752558e4159
Mvento <miquel@emevento.com>
2018-03-02 18:59:41 +0100

    tested clear empty cluster counts

--- cellphonedb/core/tests/test_query_utils.py
+++ cellphonedb/core/tests/test_query_utils.py
@@ -11,7 +11,7 @@ class TestQueryUtils(TestCase):
     FIXTURES_UTILS_PATH = '{}/utils'.format(data_test_dir)
 
     def test_apply_threshold(self):
-        cluster_counts = pd.read_csv('{}/query_utils_threshold_cluster_counts.csv'.format(self.FIXTURES_UTILS_PATH))
+        cluster_counts = pd.read_csv('{}/query_utils_generic_cluster_counts.csv'.format(self.FIXTURES_UTILS_PATH))
         expected_result = pd.read_csv('{}/query_utils_threshold_results.csv'.format(self.FIXTURES_UTILS_PATH))
 
         gene_column_name = 'gene'
@@ -20,5 +20,19 @@ class TestQueryUtils(TestCase):
         cluster_names.remove(gene_column_name)
 
         result = query_utils.apply_threshold(cluster_counts, cluster_names, threshold=0.2)
+
+        self.assertTrue(dataframe_functions.dataframes_has_same_data(result, expected_result))
+
+    def test_filter_empty_cluster_counts(self):
+        cluster_counts = pd.read_csv('{}/query_utils_generic_cluster_counts.csv'.format(self.FIXTURES_UTILS_PATH))
+        expected_result = pd.read_csv(
+            '{}/query_utils_filter_empty_cluster_results.csv'.format(self.FIXTURES_UTILS_PATH))
+
+        gene_column_name = 'gene'
+
+        cluster_names = list(cluster_counts.columns.values)
+        cluster_names.remove(gene_column_name)
+
+        result = query_utils.filter_empty_cluster_counts(cluster_counts, cluster_names)
 
         self.assertTrue(dataframe_functions.dataframes_has_same_data(result, expected_result))
