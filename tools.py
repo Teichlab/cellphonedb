5f28af675b3dd306a33741909ef54acc1ec2125c
Mvento <miquel@emevento.com>
2018-06-10 20:45:50 +0200

    WIP: renamed namefile to filename

--- tools.py
+++ tools.py
@@ -32,20 +32,20 @@ def merge_duplicated_proteins(filename):
 
 
 @cli.command()
-@click.argument('inweb_inbiomap_namefile', default='')
-@click.argument('database_proteins_namefile', default='protein.csv')
-def inweb_interactions(inweb_inbiomap_namefile, database_proteins_namefile):
-    protein_generate_inweb(inweb_inbiomap_namefile, database_proteins_namefile)
+@click.argument('inweb_inbiomap_filename', default='')
+@click.argument('database_proteins_filename', default='protein.csv')
+def inweb_interactions(inweb_inbiomap_filename, database_proteins_filename):
+    protein_generate_inweb(inweb_inbiomap_filename, database_proteins_filename)
 
 
 @cli.command()
-@click.argument('imex_original_namefile')
-@click.argument('database_proteins_namefile', default='protein.csv')
-@click.argument('database_gene_namefile', default='gene.csv')
-def imex_interactions(imex_original_namefile, database_proteins_namefile, database_gene_namefile):
-    interactions_base_df = pd.read_csv('%s/%s' % (data_dir, imex_original_namefile), sep='\t', na_values='-')
-    protein_df = pd.read_csv('%s/%s' % (data_dir, database_proteins_namefile))
-    gene_df = pd.read_csv('%s/%s' % (data_dir, database_gene_namefile))
+@click.argument('imex_original_filename')
+@click.argument('database_proteins_filename', default='protein.csv')
+@click.argument('database_gene_filename', default='gene.csv')
+def imex_interactions(imex_original_filename, database_proteins_filename, database_gene_filename):
+    interactions_base_df = pd.read_csv('%s/%s' % (data_dir, imex_original_filename), sep='\t', na_values='-')
+    protein_df = pd.read_csv('%s/%s' % (data_dir, database_proteins_filename))
+    gene_df = pd.read_csv('%s/%s' % (data_dir, database_gene_filename))
 
     result = parse_interactions_imex(interactions_base_df, protein_df, gene_df)
 
@@ -53,18 +53,18 @@ def imex_interactions(imex_original_namefile, database_proteins_namefile, databa
 
 
 @cli.command()
-@click.argument('innatedb_namefile', default='innatedb_ppi.mitab')
-@click.argument('database_gene_namefile', default='gene.csv')
-def innatedb_interactions(innatedb_namefile, database_gene_namefile):
-    generate_interactions_innatedb(innatedb_namefile, database_gene_namefile)
+@click.argument('innatedb_filename', default='innatedb_ppi.mitab')
+@click.argument('database_gene_filename', default='gene.csv')
+def innatedb_interactions(innatedb_filename, database_gene_filename):
+    generate_interactions_innatedb(innatedb_filename, database_gene_filename)
 
 
 @cli.command()
-@click.argument('interactions_namefile_1')
-@click.argument('interactions_namefile_2')
-def merge_interactions(interactions_namefile_1, interactions_namefile_2):
-    interactions_1 = _open_file(interactions_namefile_1)
-    interactions_2 = _open_file(interactions_namefile_2)
+@click.argument('interactions_filename_1')
+@click.argument('interactions_filename_2')
+def merge_interactions(interactions_filename_1, interactions_filename_2):
+    interactions_1 = _open_file(interactions_filename_1)
+    interactions_2 = _open_file(interactions_filename_2)
 
     result = merge_interactions(interactions_1, interactions_2)
 
@@ -72,53 +72,53 @@ def merge_interactions(interactions_namefile_1, interactions_namefile_2):
 
 
 @cli.command()
-@click.argument('interactions_namefile')
-@click.argument('database_complex_namefile', default='complex.csv')
-def noncomplex_interactions(interactions_namefile, database_complex_namefile):
-    interactions = pd.read_csv(_open_file(interactions_namefile))
-    complexes = pd.read_csv(_open_file(database_complex_namefile))
+@click.argument('interactions_filename')
+@click.argument('database_complex_filename', default='complex.csv')
+def noncomplex_interactions(interactions_filename, database_complex_filename):
+    interactions = pd.read_csv(_open_file(interactions_filename))
+    complexes = pd.read_csv(_open_file(database_complex_filename))
     result = only_noncomplex_interactions(interactions, complexes)
 
     result.to_csv('%s/no_complex_interactions.csv' % (output_dir), index=False, float_format='%.4f')
 
 
 @cli.command()
-@click.argument('interaction_namefile')
-@click.argument('interaction_to_remove_namefile', default='remove_interactions.csv')
-def remove_interactions(interaction_namefile, interaction_to_remove_namefile):
-    interactions = pd.read_csv(_open_file(interaction_namefile))
-    interactions_to_remove = pd.read_csv(_open_file(interaction_to_remove_namefile))
+@click.argument('interaction_filename')
+@click.argument('interaction_to_remove_filename', default='remove_interactions.csv')
+def remove_interactions(interaction_filename, interaction_to_remove_filename):
+    interactions = pd.read_csv(_open_file(interaction_filename))
+    interactions_to_remove = pd.read_csv(_open_file(interaction_to_remove_filename))
 
     result = remove_interactions_in_file(interactions, interactions_to_remove)
     result.to_csv('%s/clean_interactions.csv' % (output_dir), index=False)
 
 
 @cli.command()
-@click.argument('interaction_namefile', default='clean_interactions.csv')
-@click.argument('interaction_curated_namefile', default='interaction_curated.csv')
-def add_curated_interactions(interaction_namefile, interaction_curated_namefile):
-    interaction = pd.read_csv(_open_file(interaction_namefile))
-    interaction_curated = pd.read_csv(_open_file(interaction_curated_namefile))
+@click.argument('interaction_filename', default='clean_interactions.csv')
+@click.argument('interaction_curated_filename', default='interaction_curated.csv')
+def add_curated_interactions(interaction_filename, interaction_curated_filename):
+    interaction = pd.read_csv(_open_file(interaction_filename))
+    interaction_curated = pd.read_csv(_open_file(interaction_curated_filename))
 
     result = add_curated(interaction, interaction_curated)
     result.to_csv('%s/interaction.csv' % output_dir, index=False)
 
 
 @cli.command()
-@click.argument('imex_original_namefile')
-@click.argument('database_proteins_namefile', default='protein.csv')
-@click.argument('database_gene_namefile', default='gene.csv')
-@click.argument('database_complex_namefile', default='complex.csv')
-@click.argument('interaction_to_remove_namefile', default='remove_interactions.csv')
-@click.argument('interaction_curated_namefile', default='interaction_curated.csv')
-def generate_interactions(imex_original_namefile, database_proteins_namefile, database_gene_namefile,
-                          database_complex_namefile, interaction_to_remove_namefile, interaction_curated_namefile):
-    interactions_base = pd.read_csv('%s/%s' % (data_dir, imex_original_namefile), sep='\t', na_values='-')
-    proteins = pd.read_csv('%s/%s' % (data_dir, database_proteins_namefile))
-    genes = pd.read_csv('%s/%s' % (data_dir, database_gene_namefile))
-    complexes = pd.read_csv('%s/%s' % (data_dir, database_complex_namefile))
-    interactions_to_remove = pd.read_csv('%s/%s' % (data_dir, interaction_to_remove_namefile))
-    interaction_curated = pd.read_csv('%s/%s' % (data_dir, interaction_curated_namefile))
+@click.argument('imex_original_filename')
+@click.argument('database_proteins_filename', default='protein.csv')
+@click.argument('database_gene_filename', default='gene.csv')
+@click.argument('database_complex_filename', default='complex.csv')
+@click.argument('interaction_to_remove_filename', default='remove_interactions.csv')
+@click.argument('interaction_curated_filename', default='interaction_curated.csv')
+def generate_interactions(imex_original_filename, database_proteins_filename, database_gene_filename,
+                          database_complex_filename, interaction_to_remove_filename, interaction_curated_filename):
+    interactions_base = pd.read_csv('%s/%s' % (data_dir, imex_original_filename), sep='\t', na_values='-')
+    proteins = pd.read_csv('%s/%s' % (data_dir, database_proteins_filename))
+    genes = pd.read_csv('%s/%s' % (data_dir, database_gene_filename))
+    complexes = pd.read_csv('%s/%s' % (data_dir, database_complex_filename))
+    interactions_to_remove = pd.read_csv('%s/%s' % (data_dir, interaction_to_remove_filename))
+    interaction_curated = pd.read_csv('%s/%s' % (data_dir, interaction_curated_filename))
     print('generating imex file')
     imex_interactions = parse_interactions_imex(interactions_base, proteins, genes)
 
@@ -141,11 +141,11 @@ def generate_genes(gene_base_filename: str, remove_genes_filename: str):
     gene_actions.generate_genes_action(gene_base_filename, remove_genes_filename)
 
 
-def _open_file(interaction_namefile):
-    if os.path.isfile('%s/%s' % (data_dir, interaction_namefile)):
-        return os.path.join(data_dir, interaction_namefile)
+def _open_file(interaction_filename):
+    if os.path.isfile('%s/%s' % (data_dir, interaction_filename)):
+        return os.path.join(data_dir, interaction_filename)
 
-    return os.path.join(output_dir, interaction_namefile)
+    return os.path.join(output_dir, interaction_filename)
 
 
 if __name__ == "__main__":
