fa611bccde5ac9dcd07f8add617fd1baef59993c
Mvento <miquel@emevento.com>
2018-06-20 17:27:41 +0200

    saved iuphar processed file

--- tools/actions/interaction_actions.py
+++ tools/actions/interaction_actions.py
@@ -5,10 +5,16 @@ from tools.generate_data.parsers import parse_iuphar_guidetopharmacology
 from utils.utils import read_data_table_from_file
 
 
-def merge_iuphar_imex_action(iuphar_filename: str, gene_filename: str, protein_filename: str,
-                             imex_interactions_filename: str, data_path: str = '',
-                             result_filename: str = 'iuphar_imex_interactions.csv', result_path: str = '',
-                             download_original_path: str = '', default_download_response: str = ''):
+def merge_iuphar_imex_action(iuphar_filename: str,
+                             gene_filename: str,
+                             protein_filename: str,
+                             imex_interactions_filename: str,
+                             data_path: str = '',
+                             processed_iuphar_result_filename: str = 'cellphonedb_interactions_iuphar.csv',
+                             result_filename: str = 'iuphar_imex_interactions.csv',
+                             result_path: str = '',
+                             download_original_path: str = '',
+                             default_download_response: str = ''):
     if not result_path:
         result_path = app.output_dir
 
@@ -21,14 +27,16 @@ def merge_iuphar_imex_action(iuphar_filename: str, gene_filename: str, protein_f
     if not default_download_response:
         default_download_response = None
 
-    iuphar_original = get_iuphar_guidetopharmacology.call(iuphar_filename, data_path,
-                                                          download_original_path, default_download_response)
+    iuphar_original = get_iuphar_guidetopharmacology.call(iuphar_filename, data_path, download_original_path,
+                                                          default_download_response)
 
     genes = read_data_table_from_file('{}/{}'.format(data_path, gene_filename), dtype=str)
     proteins = read_data_table_from_file('{}/{}'.format(data_path, protein_filename), dtype=str)
     imex_interactions = read_data_table_from_file('{}/{}'.format(data_path, imex_interactions_filename), dtype=str)
 
     iuphar_interactions = parse_iuphar_guidetopharmacology.call(iuphar_original, genes, proteins)
+
+    iuphar_interactions.to_csv('{}/{}'.format(result_path, processed_iuphar_result_filename), index=False)
 
     merged_interactions = merge_interactions.merge_iuphar_imex_interactions(iuphar_interactions, imex_interactions)
 
