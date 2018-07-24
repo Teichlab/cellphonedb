c3894328ce6bdb41c44f57372d1aa7bb8fbc99fe
Mvento <miquel@emevento.com>
2018-07-24 10:43:44 +0200

    Removed uWSGI from distribute package

--- methods_refactor.py
+++
@@ -1,7 +0,0 @@
-import os
-
-current_dir = os.path.dirname(os.path.realpath(__file__))
-methods_data_test_dir = '{}/method_tests/data'.format(current_dir)
-methods_example_data = '{}/in/example_data'.format(current_dir)
-methods_input_data = '{}/methods/in'.format(current_dir)
-methods_output_data = '{}/methods/out'.format(current_dir)
