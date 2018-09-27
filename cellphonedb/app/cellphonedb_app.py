9a2b8564567a67fff3c09d60b2fde8d54ffa049b
Mvento <miquel@emevento.com>
2018-09-27 18:37:58 +0200

    source data relative to run dir

--- cellphonedb/app/cellphonedb_app.py
+++ cellphonedb/app/cellphonedb_app.py
@@ -5,9 +5,10 @@ from cellphonedb.app.flask.flask_cellphonedb import CellphonedbFlask
 cellphonedb_app = CellphonedbFlask()
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
+
 data_dir = '%s/../core/data' % current_dir
-output_dir = '%s/../../out' % current_dir
+output_dir = 'out'
+query_input_dir = 'in'
 temp_dir = '%s/../../temp' % current_dir
-query_input_dir = '%s/../../in' % current_dir
 output_test_dir = '{}/../tests/out'.format(current_dir)
 data_test_dir = '{}/../tests/fixtures'.format(current_dir)
