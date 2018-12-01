8bf63909699e9c76a64a6aea5cbe932485736dbd
Mvento <miquel@ydevs.com>
2018-12-01 09:20:58 +0100

    Added significant means analysis to non statistical method

--- cellphonedb/src/core/methods/cpdb_analysis_method.py
+++ cellphonedb/src/core/methods/cpdb_analysis_method.py
@@ -10,25 +10,31 @@ def call(meta: pd.DataFrame,
          complexes: pd.DataFrame,
          complex_compositions: pd.DataFrame,
          threshold: float = 0.1,
-         round_decimals: int = 1) -> (
-        pd.DataFrame, pd.DataFrame):
-    means_simple, deconvoluted_simple = cpdb_analysis_simple_method.call(meta.copy(),
-                                                                         counts.copy(),
-                                                                         interactions.copy(),
-                                                                         threshold,
-                                                                         round_decimals)
-    means_complex, deconvoluted_complex = cpdb_analysis_complex_method.call(meta,
-                                                                            counts,
-                                                                            interactions,
-                                                                            genes,
-                                                                            complexes,
-                                                                            complex_compositions,
-                                                                            threshold,
-                                                                            round_decimals)
+         round_decimals: int = 1) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    means_simple, significant_means_simple, deconvoluted_simple = \
+        cpdb_analysis_simple_method.call(meta.copy(),
+                                         counts.copy(),
+                                         interactions.copy(),
+                                         threshold,
+                                         round_decimals)
+    means_complex, significant_means_complex, deconvoluted_complex = \
+        cpdb_analysis_complex_method.call(meta.copy(),
+                                          counts.copy(),
+                                          interactions.copy(),
+                                          genes,
+                                          complexes,
+                                          complex_compositions,
+                                          threshold,
+                                          round_decimals)
 
     means = means_simple.append(means_complex, sort=False)
+    significant_means = significant_means_simple.append(significant_means_complex, sort=False)
     deconvoluted = deconvoluted_simple.append(deconvoluted_complex, sort=False)
+
+    max_rank = significant_means['rank'].max()
+    significant_means['rank'] = significant_means['rank'].apply(lambda rank: rank if rank != 0 else (1 + max_rank))
+    significant_means.sort_values('rank', inplace=True)
 
     if not 'complex_name' in deconvoluted:
         deconvoluted['complex_name'] = ''
-    return means, deconvoluted
+    return means, significant_means, deconvoluted
