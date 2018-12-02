c8c6280712cb6d836a917dcf4fd7588be124eece
Mvento <miquel@ydevs.com>
2018-12-02 16:48:58 +0100

    Removed unused methodo web api endpoints

--- cellphonedb/src/api_endpoints/web_api/method_web_api_endpoints/method_web_api_routes.py
+++
@@ -1,6 +0,0 @@
-from cellphonedb.src.api_endpoints.web_api.method_web_api_endpoints.web_endpoint_analysis import \
-    WebEndpointAnalysis
-
-
-def add(api, prefix=''):
-    api.add_resource(WebEndpointAnalysis, '{}/analysis'.format(prefix))
