bd139fb8bea47d91080cbb2e53e1be47a66688fb
chapuzzo <luismax@gmail.com>
2019-05-17 13:16:58 +0200

    moved plot to own subgroup

--- cellphonedb/cellphonedb_cli.py
+++ cellphonedb/cellphonedb_cli.py
@@ -2,6 +2,7 @@
 import click
 
 from cellphonedb.src.api_endpoints.terminal_api.method_terminal_api_endpoints import method_terminal_commands
+from cellphonedb.src.api_endpoints.terminal_api.plot_terminal_api_endpoints import plot_terminal_commands
 from cellphonedb.src.api_endpoints.terminal_api.query_terminal_api_endpoints import query_terminal_commands
 from cellphonedb.src.api_endpoints.terminal_api.tools_terminal_api_endpoints import tools_terminal_commands
 
@@ -26,14 +27,19 @@ def tools():
     pass
 
 
+@cli.group()
+def plot():
+    pass
+
+
 method.add_command(method_terminal_commands.statistical_analysis)
 method.add_command(method_terminal_commands.analysis)
-method.add_command(method_terminal_commands.plot)
 query.add_command(query_terminal_commands.find_interactions_by_element)
 query.add_command(query_terminal_commands.get_interaction_gene)
 query.add_command(query_terminal_commands.cpdb_data_report)
 tools.add_command(tools_terminal_commands.generate_genes)
 tools.add_command(tools_terminal_commands.generate_interactions)
+plot.add_command(plot_terminal_commands.dot_plot)
 
 if __name__ == '__main__':
     cli()
