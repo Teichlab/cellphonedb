27069f153f7cdd6488498366fea60a3f8c051697
chapuzzo <luismax@gmail.com>
2019-05-23 16:24:43 +0200

    added tool command to generate proteins list from curated and additional source

--- cellphonedb/cellphonedb_cli.py
+++ cellphonedb/cellphonedb_cli.py
@@ -39,6 +39,7 @@ query.add_command(query_terminal_commands.get_interaction_gene)
 query.add_command(query_terminal_commands.cpdb_data_report)
 tools.add_command(tools_terminal_commands.generate_genes)
 tools.add_command(tools_terminal_commands.generate_interactions)
+tools.add_command(tools_terminal_commands.generate_proteins)
 plot.add_command(plot_terminal_commands.dot_plot)
 
 if __name__ == '__main__':
