3154aadc5e2680462153451fd572047678e97318
Mvento <miquel@ydevs.com>
2019-05-25 22:19:26 +0200

    reordered protein.csv columns.

--- cellphonedb/src/core/exporters/protein_exporter.py
+++ cellphonedb/src/core/exporters/protein_exporter.py
@@ -4,11 +4,8 @@ from cellphonedb.utils import dataframe_format
 
 
 def call(proteins_expanded: pd.DataFrame) -> pd.DataFrame:
-    proteins_expanded = proteins_expanded.drop(['id_multidata', 'id_protein', 'protein_multidata_id'], axis=1)
-
     proteins_expanded.rename(index=str, columns={'name': 'uniprot'}, inplace=True)
-
-    proteins_expanded = dataframe_format.bring_columns_to_start(['uniprot'], proteins_expanded)
-    proteins_expanded = dataframe_format.bring_columns_to_end(['tags', 'tags_reason'], proteins_expanded)
-
-    return proteins_expanded
+    return proteins_expanded[
+        ['uniprot', 'protein_name', 'transmembrane', 'peripheral', 'secreted', 'secreted_desc', 'secreted_highlight',
+         'receptor', 'receptor_desc', 'integrin', 'other', 'other_desc', 'tags', 'tags_description', 'tags_reason',
+         'transporter']]
