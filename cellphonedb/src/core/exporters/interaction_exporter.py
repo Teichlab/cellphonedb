6b1c9f1698e2bfe187e11f27bdd96afef84535ae
Mvento <miquel@ydevs.com>
2019-05-26 14:00:25 +0200

    renamed protein_name_1 _2 to protein_name_a _b

--- cellphonedb/src/core/exporters/interaction_exporter.py
+++ cellphonedb/src/core/exporters/interaction_exporter.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 def call(interactions_expanded: pd.DataFrame) -> pd.DataFrame:
     interactions_expanded = interactions_expanded[interactions_expanded['is_cellphonedb_interactor']].copy()
-    interactions_expanded.rename(index=str, columns={'name_1': 'partner_a', 'name_2': 'partner_b'},
+    interactions_expanded.rename(index=str, columns={'name_1': 'partner_a',
+                                                     'name_2': 'partner_b',
+                                                     'protein_name_1': 'protein_name_a',
+                                                     'protein_name_2': 'protein_name_b'},
                                  inplace=True)
 
     return interactions_expanded[
-        ['id_cp_interaction', 'partner_a', 'partner_b', 'protein_name_1', 'protein_name_2', 'source',
+        ['id_cp_interaction', 'partner_a', 'partner_b', 'protein_name_a', 'protein_name_b', 'source',
          'comments_interaction', 'iuphar']]
