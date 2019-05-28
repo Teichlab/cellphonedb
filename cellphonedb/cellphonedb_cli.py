0c309b6b3adc16569937e714fa10a95105e667ca
chapuzzo <luismax@gmail.com>
2019-05-28 13:59:09 +0200

    created complex generation command & merge strategy

--- cellphonedb/cellphonedb_cli.py
+++ cellphonedb/cellphonedb_cli.py
@@ -40,6 +40,7 @@ query.add_command(query_terminal_commands.cpdb_data_report)
 tools.add_command(tools_terminal_commands.generate_genes)
 tools.add_command(tools_terminal_commands.generate_interactions)
 tools.add_command(tools_terminal_commands.generate_proteins)
+tools.add_command(tools_terminal_commands.generate_complex)
 plot.add_command(plot_terminal_commands.dot_plot)
 
 if __name__ == '__main__':
