8f550f9e030a57a3722d4e7cab9749b4a731c3ee
chapuzzo <luismax@gmail.com>
2019-05-29 20:15:32 +0200

    WIP: moved collet away from manage.py

--- cellphonedb/cellphonedb_cli.py
+++ cellphonedb/cellphonedb_cli.py
@@ -42,6 +42,7 @@ tools.add_command(tools_terminal_commands.generate_interactions)
 tools.add_command(tools_terminal_commands.generate_proteins)
 tools.add_command(tools_terminal_commands.generate_complex)
 tools.add_command(tools_terminal_commands.filter_all)
+tools.add_command(tools_terminal_commands.collect)
 plot.add_command(plot_terminal_commands.dot_plot)
 
 if __name__ == '__main__':
