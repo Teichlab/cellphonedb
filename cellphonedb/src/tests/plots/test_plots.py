0d1dd405683d83198a35f802419e15fa10f3b441
chapuzzo <luismax@gmail.com>
2019-06-27 16:24:21 +0200

    Added tests for heatmap_plot. Skipping suite if R setup is not available

--- cellphonedb/src/tests/plots/test_plots.py
+++ cellphonedb/src/tests/plots/test_plots.py
@@ -2,9 +2,19 @@ import os
 import unittest
 
 from cellphonedb.src.app.cellphonedb_app import output_test_dir, data_test_dir
-from cellphonedb.src.plotters.r_plotter import dot_plot
+from cellphonedb.src.exceptions.MissingR import MissingR
+from cellphonedb.src.plotters.r_plotter import dot_plot, heatmaps_plot, ensure_R_setup
 
 
+def r_available():
+    try:
+        ensure_R_setup()
+        return True
+    except MissingR:
+        return False
+
+
+@unittest.skipUnless(r_available(), 'R setup is not available in this setup')
 class TestPlots(unittest.TestCase):
     def test_dot_plot_with_all_columns_and_rows(self):
         means_path = self._get_input_file_path_for('means')
@@ -40,6 +50,25 @@ class TestPlots(unittest.TestCase):
         self.assertTrue(os.path.exists(output_path), 'Plot of type dot_plot did not work')
         self.assertGreater(os.path.getsize(output_path), 0)
 
+    def test_heatmaps_plot(self):
+        meta_path = self._get_fixture('hi_test_meta.txt')
+        pvalues_path = self._get_input_file_path_for('pvalues')
+        plot_output_counts = 'heatmaps_plot_counts.png'
+        plot_output_count_logs = 'heatmaps_plot_count_logs.png'
+
+        heatmaps_plot(meta_file=meta_path,
+                      pvalues_file=pvalues_path,
+                      output_path=output_test_dir,
+                      count_name=plot_output_counts,
+                      log_name=plot_output_count_logs,
+                      pvalue=0.05
+                      )
+
+        for file in [plot_output_counts, plot_output_count_logs]:
+            output_path = os.path.join(output_test_dir, file)
+            self.assertTrue(os.path.exists(output_path), 'Plot of type heatmap_plot did not work')
+            self.assertGreater(os.path.getsize(output_path), 0)
+
     @staticmethod
     def _get_input_file_path_for(kind):
         template = 'input_for_plot_statistical_analysis__{}_result__data-test_it-10_seed-0_threshold-01_precision-1.txt'
@@ -49,4 +78,3 @@ class TestPlots(unittest.TestCase):
     @staticmethod
     def _get_fixture(name):
         return os.path.join(data_test_dir, name)
-
