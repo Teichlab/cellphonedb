9d726e628a7d246cbc9aa3d54106d89de090bcbb
Mvento <miquel@ydevs.com>
2019-09-05 20:41:19 +0200

    v2.1.0 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='CellPhoneDB',
     author='TeichLab',
     author_email='contact@cellphonedb.org',
-    version='2.0.6',
+    version='2.1.0',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
