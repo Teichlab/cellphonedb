87c1d31d185e1e7e2d341dc71e99762cee04914d
Stefan Lang <Stefan.Lang@med.lu.se>
2020-11-26 11:48:22 +0100

    removing strict version requrements for packages

--- setup.py
+++ setup.py
@@ -21,19 +21,19 @@ setup(
             ]
     },
     install_requires=[
-        'click>=6.7,<6.7.99',
-        'pandas>=0.23,<0.23.99',
-        'flask>=1.0,<1.0.99',
-        'Flask-RESTful>=0.3,<0.3.99',
-        'Flask-Testing>=0.7,<0.7.99',
-        'SQLAlchemy>=1.3,<1.3.99',
-        'PyYAML>=5.1,<5.1.99',
-        'requests>=2.19,<2.19.99',
-        'pika>=0.12,<0.12.99',
-        'boto3>=1.7,<1.7.99',
+        'click>=6.7',
+        'pandas>=0.23',
+        'flask>=1.0',
+        'Flask-RESTful>=0.3',
+        'Flask-Testing>=0.7',
+        'SQLAlchemy>=1.3',
+        'PyYAML>=5.1',
+        'requests>=2.19',
+        'pika>=0.12',
+        'boto3>=1.7',
         'geosketch==0.3',
-        'rpy2>=3.0.4,<3.0.99',
-        'tqdm>=4.32,<4.32.99',
-        'cython>=0.29,<0.29.99'
+        'rpy2>=3.0.4',
+        'tqdm>=4.32',
+        'cython>=0.29'
     ],
 )
